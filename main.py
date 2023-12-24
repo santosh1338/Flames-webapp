d2c653787916368d6476a2674d201dbd3dd13bbb
santosh1338 <78336354+santosh1338@users.noreply.github.com>
2023-12-24 19:33:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,22 +20,27 @@ if button:
     boy = list(B.lower())
     girl = list(G.lower())
 
-    while ' ' in boy:
-        boy.pop(boy.index(' '))
+    while ' ' in boy:  # removing all the empty spaces
+    boy.pop(boy.index(' '))
     while ' ' in girl:
         girl.pop(girl.index(' '))
-    for i in range(len(boy)-1,-1,-1):                               
+
+    for i in range(len(boy) - 1, -1, -1):  # removing all the matched characters
         if boy[i] in girl:
-         girl.pop(girl.index(boy[i]))
-         boy.pop(i)
-    num = len(boy + girl)
+            # print(boy[i],'true')
+            girl.pop(girl.index(boy[i]))
+            boy.pop(i)
+            # print(boy,girl)
+    trans = boy + girl
+    # print(trans,len(trans))#i dont really mean it, it is just fun and games
+    num = len(trans)  # list of unmatched characters
+    time.sleep(1)  # added time for that extra bit of suspense
     flames = ['F', 'L', 'A', 'M', 'E', 'S']
-    a = 0
-    time.sleep(2)
+    a = 0  # stores the index of the previously removed character
     for love in range(6, 1, -1):
-        s = num % love - 1
+        s = num % love - 1  # index no of the element to be removed
         st.write(popped[flames[(s + a) % love]])
         flames.pop((s + a) % love)
-        a = s
-        time.sleep(2)
+        a = s  # this is so that after restart the loop and land on a value,and u move to the next 'a'th element
+        time.sleep(1.5)  # cooldown time, to build suspence
     st.title(result[flames[0]])
